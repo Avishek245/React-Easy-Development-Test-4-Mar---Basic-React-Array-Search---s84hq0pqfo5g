138c2c8f11e7d6b92721b9116cdf59feed6883d7
Avishek245 <96076589+Avishek245@users.noreply.github.com>
2022-03-04 19:28:21 +0530

    "Assignment"

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import '../styles/App.css';
+import "../styles/App.css";
 
-
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
+const searchArray = [
+  "Newton",
+  "School",
+  "Newton School",
+  "React",
+  "Preact",
+  "Node",
+  "Mongoose",
+  "Angular",
+  "Vue",
+  "Apple",
+  "Microsoft",
+  "Netflix",
+  "Meta"
+];
 const App = () => {
-
-  
+  const [search, setSearchItem] = useState("");
   return (
     <div id="main">
-      
+      <h2>Search</h2>
+      <input
+        type="text"
+        id="search-input"
+        onChange={(event) => setSearchItem(event.target.value)}
+      />
+      <h2>Result </h2>
+      {searchArray
+        .map((value, key) => {
+          return <div>{value}</div>;
+        })
+        .filter((value) => {
+          if (value.toLowerCase().includes(search.toLowerCase())) {
+            return value;
+          }
+        })}
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
